aa25403080fa32e42c276e2a4e96b98975cdca11
KoTeD0 <darkest.jerry@gmail.com>
2022-06-05 20:29:24 +0300

    initital commit

---
+++ Mega_Market/__init__.py
@@ -0,0 +1,18 @@
+"""
+Сервис с REST API, для сравнения цен, аналогичный сервису Яндекс Товары
+"""
+
+__author__ = 'Atayev Rejep'
+__maintainer__ = __author__
+
+__email__ = 'darkest.jerry@gmail.com'
+__licence__ = 'MIT'
+__version__ = '0.0.1'
+
+__all__ = (
+    '__author__',
+    '__email__',
+    '__licence__',
+    '__maintainer__',
+    '__version__',
+)
---
+++ setup.py
@@ -0,0 +1,54 @@
+import os
+from importlib.machinery import SourceFileLoader
+
+from pkg_resources import parse_requirements
+from setuptools import find_packages, setup
+
+module_name = 'Mega_Market'
+
+module = SourceFileLoader(
+    module_name, os.path.join(module_name, '__init__.py')
+).load_module()
+
+
+def load_requirements(fname: str) -> list:
+    requirements = []
+    with open(fname, 'r') as fp:
+        for req in parse_requirements(fp.read()):
+            extras = '[{}]'.format(','.join(req.extras)) if req.extras else ''
+            requirements.append('{}{}{}'.format(req.name, extras, req.specifier))
+    return requirements
+
+
+setup(
+    name=module_name,
+    version=module.__version__,
+    author=module.__author__,
+    author_email=module.__email__,
+    license=module.__licence__,
+    description=module.__doc__,
+    long_description=open('README.rst').read(),
+    url='https://github.com/KoTeD0/Yandex_MegaMarket_2022',
+    platforms='all',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: Russian',
+        'Operating System :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ],
+
+    python_requires='>=3.10',
+    packages=find_packages(exclude=['tests']),
+    install_requires=load_requirements('requirements.txt'),
+    extras_require={'dev': load_requirements('requirements.dev.txt')},
+    entry_points={
+        'console_scripts': [
+            '{0}-api = {0}.api.__main__:main'.format(module_name),
+            '{0}-db = {0}.db.__main__:main'.format(module_name)
+        ]
+    },
+    include_package_data=True
+)

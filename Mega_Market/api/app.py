8ec0c7b8371facc89ce056322a1a277081ec319c
KoTeDo <darkest.jerry@gmail.com>
2022-06-18 18:43:29 +0300

    getting by node and deleting partially works

--- Mega_Market/api/app.py
+++ Mega_Market/api/app.py
@@ -1,14 +1,15 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from Mega_Market.api.database import SessionLocal, engine
-from . import schemas, models
+from . import schema, model
 
 import datetime
+from uuid import UUID
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+model.Base.metadata.create_all(bind=engine)
 
 
 def get_db():
@@ -19,8 +20,8 @@ def get_db():
         db.close()
 
 
-def shop_unit_to(shop_unit: schemas.ShopUnitImport, date: datetime):
-    return models.ShopUnit(
+def shop_unit_to(shop_unit: schema.ShopUnitImport, date: datetime):
+    return model.ShopUnit(
         id=shop_unit.id,
         name=shop_unit.name,
         parentId=shop_unit.parentId,
@@ -31,8 +32,21 @@ def shop_unit_to(shop_unit: schemas.ShopUnitImport, date: datetime):
 
 
 @app.post('/imports')
-def import_shop_unit(shop_unit_request: schemas.ShopUnitImportRequest, db: Session = Depends(get_db)):
+def import_shop_unit(shop_unit_request: schema.ShopUnitImportRequest, db: Session = Depends(get_db)):
     for shop_unit in shop_unit_request.items:
         unit = shop_unit_to(shop_unit, shop_unit_request.updateDate)
         db.add(unit)
     db.commit()
+
+
+@app.delete('/delete/{shop_unit_id}')
+def delete_id(shop_unit_id: UUID, db: Session = Depends(get_db)):
+    shop_unit = db.query(model.ShopUnit).filter(model.ShopUnit.id == shop_unit_id).delete()
+    if not shop_unit:
+        raise HTTPException(status_code=404, detail='Item not found')
+    db.commit()  # TODO: удалять категории и все дочерние элементы
+
+
+@app.get('/nodes/{shop_unit_id}')
+def get_shop_unit_by_id(shop_unit_id: UUID, db: Session = Depends(get_db)):
+    return db.query(model.ShopUnit).filter(model.ShopUnit.id == shop_unit_id).first()

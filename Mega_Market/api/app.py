10a59ab63c02d6badcedcdf0cac7b0c79f64a405
KoTeDo <darkest.jerry@gmail.com>
2022-06-18 14:11:26 +0300

    basic import added and tested

--- Mega_Market/api/app.py
+++ Mega_Market/api/app.py
@@ -1,7 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from Mega_Market.api.database import SessionLocal, engine
+from . import schemas, models
+
+import datetime
 
 app = FastAPI()
 
-@app.get('/')
-def hello(limit:int):
-    return {"limit": f'specified limit parameter = {limit}'}
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def shop_unit_to(shop_unit: schemas.ShopUnitImport, date: datetime):
+    return models.ShopUnit(
+        id=shop_unit.id,
+        name=shop_unit.name,
+        parentId=shop_unit.parentId,
+        price=shop_unit.price,
+        date=date,
+        type=shop_unit.type,
+    )
+
+
+@app.post('/imports')
+def import_shop_unit(shop_unit_request: schemas.ShopUnitImportRequest, db: Session = Depends(get_db)):
+    for shop_unit in shop_unit_request.items:
+        unit = shop_unit_to(shop_unit, shop_unit_request.updateDate)
+        db.add(unit)
+    db.commit()
---
+++ Mega_Market/api/models.py
@@ -0,0 +1,33 @@
+import uuid
+
+from sqlalchemy import Column, String, Integer, DateTime, Enum, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm.collections import attribute_mapped_collection
+
+from .database import Base
+
+
+class ShopUnit(Base):
+    __tablename__ = 'shop_unit'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(String(100), nullable=False)
+    date = Column(DateTime, nullable=False)
+    parentId = Column(UUID(as_uuid=True), ForeignKey(id), nullable=True)
+    type = Column(Enum('OFFER', 'CATEGORY', name='shop_unit_type'), nullable=False)
+    price = Column(Integer)
+    children = relationship('ShopUnit',
+        cascade='all, delete-orphan',
+        backref=backref('parent', remote_side=id),
+        collection_class=attribute_mapped_collection('name'),
+    )
+
+    def __init__(self, id, name, price, date, type, parentId):
+        self.id = id
+        self.name = name
+        self.parentId = parentId
+        self.price = price
+        self.date = date
+        self.type = type
+
